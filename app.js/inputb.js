0640d44dc15007a56eff27b6ba9c562fb7f63708
mahendra761 <mahendragawande90@gmail.com>
2024-01-04 18:54:19 +0530

    m4hv

---
+++ app.js/inputb.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    // Replace datetime input with a date-time picker using a library like jQuery UI or others
+    $('input[type=datetime]').datepicker(); // Example using jQuery UI datepicker
+  });
